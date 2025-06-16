db3bfa779545b31cabf6318170c7344cad5edfd2
Melwyn Peter <melpeter377@gmail.com>
2025-06-16 17:39:03 +0530

    fix sign in aqnd up

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,19 @@
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import React, { useState } from 'react'
 import { auth } from '../services/firebase'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Login = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const navigate = useNavigate();
 
     const handleLogin = async (e) => {
         e.preventDefault()
         try{
             await signInWithEmailAndPassword(auth, email, password);
             alert("Login Successful")
+            navigate("/admin")
         }catch(err){
             console.log(err.message)
         }
@@ -22,6 +25,7 @@ const Login = () => {
             <input value={email} onChange={e => setEmail(e.target.value)} placeholder='Email' />
             <input value={password} onChange={e => setPassword(e.target.value)} placeholder='Password' />
             <button type='submit'>Login</button>
+            <p>Don't have an account? <Link to="/signup">Sign up</Link></p>
         </form>
     </div>
   )
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,16 +1,19 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react'
 import { auth } from '../services/firebase';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Signup = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
     const handleSignup = async (e) => {
         e.preventDefault();
         try{
             await createUserWithEmailAndPassword(auth, email, password);
             alert("Sigup successful")
+            navigate("/login")
         } catch(err){
             console.log(err.message)
         }
@@ -22,6 +25,7 @@ const Signup = () => {
             <input value={email} onChange={e => setEmail(e.target.value)} placeholder='Email' />
             <input value={password} onChange={e => setPassword(e.target.value)} placeholder='Password' />
             <button type='submit'>Sign Up</button>
+            <p>Already have an account? <Link to="/login">Login</Link></p>
         </form>
     </div>
   )
